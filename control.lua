ee3fb371c4aebfdb55e8f07280771a42fccabb91
IBlackI <lennard@rottjers.nl>
2017-04-23 16:29:30 +0200

    Added an control.lua with all modules and a description of those, uncommented all require to force sheeple to configure to their needs.

--- control.lua
+++ control.lua
@@ -1,21 +1,20 @@
-require "util"
-require "locale/utils/event"
-require "config"
-require "locale/utils/admin"
-require "locale/utils/undecorator"
-require "locale/utils/utils"
-require "locale/utils/gravestone"
-require "announcements"
-require "bps"
-require "tag"
-require "locale/utils/patreon"
-require "rocket"
-require "grid"
-
+--require "locale/utils/event" --Yes this line is seriously commented out, and yes without this it won't even work. I guess you now HAVE TO change the settings to match what you need.
+--require "config" --config which is mainly used by the announcements
+--require "locale/utils/admin"	--Admin module to give the admins spectate, commands and character modifications.
+--require "locale/utils/undecorator"	--Removes decorations
+--require "locale/utils/utils"	--Old 3ra stuff... Dunno know
+--require "locale/utils/gravestone"	--Module to generate a chest on player death with his/hers items
+--require "announcements"	--Module to announce stuff ingame / give the players a welcome message
+--require "bps"	--BluePrintString a module in which the player can enter a string and a blueprint get generated out of it.
+--require "tag" --Module to let players set a tag behind their names to improve teamwork
+--require "locale/utils/patreon" --Module to give patreons spectate and a nice unique tag
+--require "rocket" --Module to stop people removing the rocket silo
+--require "grid" --Worldgenerator which devides the world into a grid.
+--require "void" --Worldgenerator which randomly generates holes in the world
+--require "stats" --Module to generate stats and print them to the filesystem
 
 
 -- Give player starting items.
--- Gives admins a tool
 -- @param event on_player_joined event
 function player_joined(event)
 	local player = game.players[event.player_index]
