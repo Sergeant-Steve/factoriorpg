2d3ef94671bb56c756ca205057564ca3e2a790b7
IBlackI <lennard@rottjers.nl>
2017-04-23 17:14:14 +0200

    Deleted old 3ra files

--- config.lua
+++
@@ -1,7 +0,0 @@
--- A 3Ra Gaming compilation
-Event.register(-1, function()
-    global.scenario = global.senario or {}
-    global.scenario.config = {}
-    global.scenario.config.announcements_enabled = true -- if true announcements will be shown
-    global.scenario.config.announcement_delay = 600 -- number of seconds between each announcement
-end)
