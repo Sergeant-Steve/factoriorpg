75c32523931dfe9d5d908c47517cb9683c7885cb
IBlackI <lennard@rottjers.nl>
2017-04-24 13:49:21 +0200

    Updated to follow naming convention

--- tag.lua
+++ tag.lua
@@ -2,15 +2,16 @@
 -- A 3Ra Gaming creation
 -- Modified by I_IBlackI_I
 
-function create_tag_gui(event)
+function tag_create_gui(event)
 	local player = game.players[event.player_index]
 	if player.gui.top.tag == nil then
-		player.gui.top.add { name = "tag", type = "button", caption = "Tag" }
+		player.gui.top.add { name = "rules_tag", type = "button", caption = "Tag" }
 	end
 end
 
 -- Tag list
-roles = {
+global.tag = global.tag or {}
+global.tag.tags = {
 	{ display_name = "Mining" },
 	{ display_name = "Oil" },
 	{ display_name = "Bus" },
@@ -26,42 +27,42 @@ roles = {
 	{ display_name = "Clear" }
 }
 
-function expand_tag_gui(player)
+function tag_expand_gui(player)
 	local frame = player.gui.left["tag-panel"]
 	if (frame) then
 		frame.destroy()
 	else
 		local frame = player.gui.left.add { type = "frame", name = "tag-panel", caption = "Choose Tag" }
-		for _, role in pairs(roles) do
-			frame.add { type = "button", caption = role.display_name, name = role.display_name }
+		for _, role in pairs(global.tag.tags) do
+			frame.add { type = "button", caption = role.display_name, name = "rules_" .. role.display_name }
 		end
 	end
 end
 
 
 
-local function on_gui_click(event)
+function tag_on_gui_click(event)
 	if not (event and event.element and event.element.valid) then return end
 	local player = game.players[event.element.player_index]
 	local name = event.element.name
-
-	if (name == "tag") then
-		expand_tag_gui(player)
+	if (name == "rules_tag") then
+		tag_expand_gui(player)
 	end
 
-	if (name == "Clear") then
+	if (name == "rules_Clear") then
 		player.tag = ""
-		expand_tag_gui(player)
+		tag_expand_gui(player)
 		return
 	end
-	for _, role in pairs(roles) do
-		if (name == role.display_name) then
+	
+	for _, role in pairs(global.tag.tags) do
+		if (name == "rules_" .. role.display_name) then
 			player.tag = "[" .. role.display_name .. "]"
-			expand_tag_gui(player)
+			tag_expand_gui(player)
 		end
 	end
 end
 
 
-Event.register(defines.events.on_gui_click, on_gui_click)
-Event.register(defines.events.on_player_joined_game, create_tag_gui)
+Event.register(defines.events.on_gui_click, tag_on_gui_click)
+Event.register(defines.events.on_player_joined_game, tag_create_gui)
