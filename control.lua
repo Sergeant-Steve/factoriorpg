6724b65c218dfe65ef3bbcdce72a9c3146ec9cdf
viceroypenguin <stuart@turner-isageek.com>
2017-05-09 18:42:49 -0500

    Add tech specific material spawn

--- control.lua
+++ control.lua
@@ -23,23 +23,48 @@
 
 -- FOLLOWING CODE GIVES SAME MINIMUM INVENTORY TO ALL SCENARIOS:
 
+local function ticks_from_minutes(minutes)
+	return minutes * 60 * 60
+end
+
 -- Give player starting items.
 -- @param event on_player_joined event
 function player_joined(event)
 	local player = game.players[event.player_index]
-	player.insert { name = "iron-plate", count = 8 }
-	player.insert { name = "pistol", count = 1 }
-	player.insert { name = "firearm-magazine", count = 20 }
-	player.insert { name = "burner-mining-drill", count = 2 }
-	player.insert { name = "stone-furnace", count = 2 }
+	if game.tick < ticks_from_minutes(10) then
+		player.insert { name = "pistol", count = 1 }
+		player.insert { name = "firearm-magazine", count = 20 }
+		player.insert { name = "burner-mining-drill", count = 2 }
+		player.insert { name = "stone-furnace", count = 2 }
+	end
+
+	if (player.force.technologies["steel-processing"].researched) then
+        player.insert { name = "steel-axe", count = 2 }
+    else
+        player.insert { name = "axe", count = 5 }
+    end
 end
 
 -- Give player weapons after they respawn.
 -- @param event on_player_respawned event
 function player_respawned(event)
 	local player = game.players[event.player_index]
-	player.insert { name = "pistol", count = 1 }
-	player.insert { name = "firearm-magazine", count = 10 }
+
+	if (player.force.technologies["military"].researched) then
+        player.insert { name = "submachine-gun", count = 1 }
+    else
+		player.insert { name = "pistol", count = 1 }
+    end
+
+	if (player.force.technologies["uranium-ammo"].researched) then
+        player.insert { name = "uranium-rounds-magazine", count = 10 }
+    else 
+		if (player.force.technologies["military-2"].researched) then
+			player.insert { name = "piercing-rounds-magazine", count = 10 }
+		else
+			player.insert { name = "firearm-magazine", count = 10 }
+		end
+	end
 end
 
 Event.register(defines.events.on_player_created, player_joined)
