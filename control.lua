2f856cc105d89a864f6ce045a6c7b16b288b38af
IBlackI <lennard@rottjers.nl>
2017-05-10 20:52:02 +0200

    Revert "Add island_spawn scenario"
    This reverts commit 37bb04cc2500b2e1b41da8fdb190e42acc5c9d98.

--- control.lua
+++ control.lua
@@ -1,4 +1,3 @@
--- TOOLS: Recommend all be turned on
 --require "locale/utils/event" --Yes this line is seriously commented out, and yes without this it won't even work. I guess you now HAVE TO change the settings to match what you need.
 --require "permissions" --Permission manager
 --require "trusted" --Module to add trusted players to a seperate permission group
@@ -6,22 +5,25 @@
 --require "locale/utils/admin"	--Admin module to give the admins spectate, commands and character modifications.
 --require "announcements"	--Module to announce stuff ingame / give the players a welcome message
 --require "tag" --Module to let players set a tag behind their names to improve teamwork
+--require "void" --Worldgenerator which randomly generates holes in the world
+--require "nuclear" --worldgenerator for nuclear scenario
 --require "fmcd" --Module to consolidate saving data to an output file for the agent
 --require "stats" --Module to generate stats and print them to the filesystem
 --require "popup" --Module to create and display an popup in the center of all players their screens.
 --require "rules" --Module which displays a popup with the rules when a player joins, or presses the open rules button
 --require "rocket" --Module to stop people removing the rocket silo
 
--- World Generators: Pick only ONE
---require "void" --Worldgenerator which randomly generates holes in the world
---require "nuclear" --worldgenerator for nuclear scenario
---require "island_spawn" --worldgenerator for island spawn scenario
---require "grid" --Worldgenerator which devides the world into a grid.
+--OBSOLETE AS OF 0.15
+--require "locale/utils/gravestone"	--Module to generate a chest on player death with his/hers items
+--require "bps"	--BluePrintString a module in which the player can enter a string and a blueprint get generated out of it.
 
 --BROKEN?
 --require "locale/utils/undecorator"	--Removes decorations
 
--- FOLLOWING CODE GIVES SAME MINIMUM INVENTORY TO ALL SCENARIOS:
+--NOT UPDATED YET
+
+--require "grid" --Worldgenerator which devides the world into a grid.
+
 
 local function ticks_from_minutes(minutes)
 	return minutes * 60 * 60
@@ -66,6 +68,7 @@ function player_respawned(event)
 		end
 	end
 end
+
 
 Event.register(defines.events.on_player_created, player_joined)
 Event.register(defines.events.on_player_respawned, player_respawned)
