3dacbd8f242c660117c5a1f463deb7c93df7e8c9
viceroypenguin <stuart@turner-isageek.com>
2017-05-25 22:47:27 -0500

    Wave Defense works.

--- control.lua
+++ control.lua
@@ -1,5 +1,5 @@
 -- TOOLS: Recommend all be turned on
-require "mod-gui"
+--require "mod-gui"
 --require "locale/utils/event" --Yes this line is seriously commented out, and yes without this it won't even work. I guess you now HAVE TO change the settings to match what you need.
 --require "permissions" --Permission manager
 --require "trusted" --Module to add trusted players to a seperate permission group
@@ -17,53 +17,8 @@ require "mod-gui"
 --NOT UPDATED require "void" --Worldgenerator which randomly generates holes in the world
 --require "nuclear" --worldgenerator for nuclear scenario
 --NOT UPDATED require "grid" --Worldgenerator which devides the world into a grid.
-
+--require "wave_defense" -- move all files from wave-defense folder to root in order to use
 
 -- FOLLOWING CODE GIVES SAME MINIMUM INVENTORY TO ALL SCENARIOS:
-
-local function ticks_from_minutes(minutes)
-	return minutes * 60 * 60
-end
-
--- Give player starting items.
--- @param event on_player_joined event
-function player_joined(event)
-	local player = game.players[event.player_index]
-	if game.tick < ticks_from_minutes(10) then
-		player.insert { name = "pistol", count = 1 }
-		player.insert { name = "firearm-magazine", count = 20 }
-		player.insert { name = "burner-mining-drill", count = 2 }
-		player.insert { name = "stone-furnace", count = 2 }
-	end
-
-	if (player.force.technologies["steel-processing"].researched) then
-        player.insert { name = "steel-axe", count = 2 }
-    else
-        player.insert { name = "iron-axe", count = 5 }
-    end
-end
-
--- Give player weapons after they respawn.
--- @param event on_player_respawned event
-function player_respawned(event)
-	local player = game.players[event.player_index]
-
-	if (player.force.technologies["military"].researched) then
-        player.insert { name = "submachine-gun", count = 1 }
-    else
-		player.insert { name = "pistol", count = 1 }
-    end
-
-	if (player.force.technologies["uranium-ammo"].researched) then
-        player.insert { name = "uranium-rounds-magazine", count = 10 }
-    else 
-		if (player.force.technologies["military-2"].researched) then
-			player.insert { name = "piercing-rounds-magazine", count = 10 }
-		else
-			player.insert { name = "firearm-magazine", count = 10 }
-		end
-	end
-end
-
-Event.register(defines.events.on_player_created, player_joined)
-Event.register(defines.events.on_player_respawned, player_respawned)
+-- turn off when using wave_defense
+require "equipment"
