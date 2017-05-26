779c86f425ea1e68ec6e2950a97a6149d165d69a
Brickcaster <dralon@yahoo.com>
2017-05-26 09:00:53 -0400

    Added permission control
    Limited the ability to edit and delete tags for experienced players.

--- rpg_permissions.lua
+++ rpg_permissions.lua
@@ -2,6 +2,8 @@ function rpg_permissions_init()
 	local default = game.permissions.groups[1]
 	default.set_allows_action(defines.input_action.deconstruct, false)
 	default.set_allows_action(defines.input_action.change_programmable_speaker_parameters, false)
+	default.set_allows_action(defines.input_action.edit_custom_tag, false)
+	default.set_allows_action(defines.input_action.delete_custom_tag, false)
 	game.permissions.create_group("trusted") --For level 5+ players.
 end
 
