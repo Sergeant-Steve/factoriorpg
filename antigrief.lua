5835cdf00ad8b89f9fa560b180e342f70e7178eb
Brickcaster <dralon@yahoo.com>
2017-07-02 13:15:04 -0400

    Sigificant improvements.  Added hoarding detection, improved decon detection, added atomic bomb detection

--- antigrief.lua
+++ antigrief.lua
@@ -2,8 +2,14 @@
 --Written by Mylon
 --MIT License
 
+antigrief = {}
+global.antigrief_cooldown = {}
+
+antigrief.TROLL_TIMER = 60 * 60 * 30 --30 minutes.  Players must be online this long to not throw some warnings.
+antigrief.SPAM_TIMER = 60 * 60 * 2 --10 minutes.  Limit inventory related messages to once per 10m.
+
 --Print text to online admins and write to the log.
-function antigrief_alert(text)
+function antigrief.alert(text)
     for n, p in pairs(game.players) do
         if p.admin then
             p.print(text)
@@ -13,40 +19,117 @@ function antigrief_alert(text)
 end
 
 --Common tactic is to remove pump.  So if someone landfills a pump and removes it... That's a huge red flag.
-function antigrief_pump(event)
+function antigrief.pump(event)
     if not event.entity and not event.entity.valid then
         return
     end
     --Only check for entities in a specific list.
-    if antigrief_is_well_pump(event.entity) then
+    if antigrief.is_well_pump(event.entity) then
         local player = game.players[event.player_index]
-        antigrief_alert(player.name .. " has mined a well-water pump.")
+        antigrief.alert(player.name .. " has mined a well-water pump.")
     end
 end
 
 --When someone decons > 50 entities, fire an alert
-function antigrief_decon(event)
+function antigrief.decon(event)
     if event.alt then --This is a cancel order.
         return
     end
     local player = game.players[event.player_index]
     local count = player.surface.count_entities_filtered{area=event.area, force=player.force}
     if count >= 50 then
-        antigrief_alert(player.name .. "has deconstructed 50+ entities.")
-        return
+        --Need a proper check of entities.  Most might be filtered out and not actually deconned.
+        local ents = player.surface.find_entities_filtered{area=event.area, force=player.force}
+        count = 0
+        for k, v in pairs(ents) do
+            if v.to_be_deconstructed(player.force) then
+                count = count + 1
+            end
+        end
+        if count >= 50 then
+            antigrief.alert(player.name .. " has deconstructed ".. count .. " entities.")
+            return
+        end
     end
     --Check to see if a off-shore pump was targetted.
     local ents = player.surface.find_entities_filtered{area=event.area, force=player.force, name="offshore-pump"}
     for k, v in pairs(ents) do
-
-        if antigrief_is_well_pump(v) then
-            antigrief_alert(player.name .. " has deconned a well-water pump.")
+        if v.to_be_deconstructed(player.force) and antigrief.is_well_pump(v) then
+            antigrief.alert(player.name .. " has marked a well-water pump for deconstruction")
             return
         end
     end
 end
 
-function antigrief_is_well_pump(entity)
+--If new players equip an atomic bomb... Throw a warning!
+function antigrief.da_bomb(event)
+    local player = game.players[event.player_index]
+    if player.online_time > antigrief.TROLL_TIMER then
+        return
+    end
+    if player.get_item_count("atomic-bomb") > 0 then
+        if antigrief.check_cooldown(event.player_index) then
+            antigrief.alert(player.name .. " has equipped an Atomic Bomb.")
+        end
+    end
+end
+
+--Look for players hoarding high value items.
+function antigrief.hoarder(event)
+    local player = game.players[event.player_index]
+    if player.online_time > antigrief.TROLL_TIMER then
+        return
+    end
+    if antigrief.check_cooldown(event.player_index) then
+        if player.get_item_count("speed-module-3") > 10 or
+        player.get_item_count("productivity-module-3") > 10 or
+        player.get_item_count("effectivity-module-3") > 10 then
+            antigrief.alert(player.name .. " is hoarding T3 modules.")
+        end
+        if player.get_item_count("uranium-235") > 10 then
+            antigrief.alert(player.name .. " is hoarding U-235.")
+        end
+        if player.get_item_count("power-armor-mk2") >= 2 then
+            antigrief.alert(player.name.. " is hoarding power armor mk2s.")
+        end
+    end
+end
+
+--Did someone craft/request Mk2 power armor and then log out?
+function antigrief.armor_drop(event)
+    local player = game.players[event.player_index]
+    if player.online_time > antigrief.TROLL_TIMER then
+        return
+    end
+    if player.get_item_count("power-armor-mk2") >= 1 then
+        local armor = player.get_inventory(defines.inventory.player_armor).find_item_stack("power-armor-mk2") or
+        player.get_inventory(defines.inventory.player_main).find_item_stack("power-armor-mk2") or
+        player.get_inventory(defines.inventory.player_quickbar).find_item_stack("power-armor-mk2") or
+        player.get_inventory(defines.inventory.player_trash).find_item_stack("power-armor-mk2")
+
+        if armor then
+            local item = player.surface.spill_item_stack(player.position, armor) --This could be used to duplicate equipment if we remove the wrong PA2.  But such a weird edge case...
+            player.remove_item("power-armor-mk2")
+            item.order_deconstruction(player.force)
+        else --Something went wrong.  We should have found the armor.  God inventory?
+            log("Antigrief: Power Armor mk2 detected but not found")
+        end
+    end           
+end
+
+--Check if a message has been generated about this player recently.  If true, set cooldown.
+function antigrief.check_cooldown(player_index)
+    local cooldown = global.antigrief_cooldown[player_index]
+    if (not cooldown) or cooldown < game.tick then
+        global.antigrief_cooldown[player_index] = game.tick + antigrief.SPAM_TIMER
+        return true
+    else
+        return false
+    end
+end
+
+--Is this a water-well pump?
+function antigrief.is_well_pump(entity)
     if entity.name ~= "offshore-pump" then
         return false
     end
@@ -57,9 +140,10 @@ function antigrief_is_well_pump(entity)
 
         return true
     end
-
 end
 
-
-Event.register(defines.events.on_player_mined_entity, antigrief_pump)
-Event.register(defines.events.on_player_deconstructed_area, antigrief_decon)
+Event.register(defines.events.on_player_ammo_inventory_changed, antigrief.da_bomb)
+Event.register(defines.events.on_player_main_inventory_changed, antigrief.hoarder)
+Event.register(defines.events.on_player_left_game, antigrief.armor_drop)
+Event.register(defines.events.on_player_mined_entity, antigrief.pump)
+Event.register(defines.events.on_player_deconstructed_area, antigrief.decon)
