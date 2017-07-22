00e95443c2974ff21ddb2a574df888b8f061d0e3
Blackstone <IBlackI@users.noreply.github.com>
2017-07-22 13:55:33 +0200

    Initial version of modular_admin_ghosts

--- locale/utils/modular_admin_ghosts.lua
+++ locale/utils/modular_admin_ghosts.lua
@@ -20,30 +20,73 @@ global.modular_admin_ghosts.enabled = true
 --
 --	FUNCTIONS
 --
-function modular_admin_ghosts_enable(mod)
+function modular_admin_ghosts_create_force()
+	if not game.forces.Admins then
+		game.create_force("Admins")
+		game.forces.Admins.research_all_technologies()
+	end
+end
+
+function modular_admin_ghosts_entity_mined(event)
+	local entity = event.entity
+	if entity.force.name == "neutral" 
+	or entity.name == "entity-ghost" 
+	or entity.type == "locomotive" 
+	or entity.type == "cargo-wagon" 
+	or entity.type == "fluid-wagon"
+	or entity.type == "car" 
+	or entity.type:find("robot") 
+	or game.players[event.player_index].force == game.forces.Admins 
+	or entity.name == "tile-ghost"
+    or entity.name == 'item-request-proxy'
+	then return end
+	local ghost = "a"
+	if (game and game.active_mods.base:sub(1,4) == '0.14' and (entity.type == 'underground-belt' or entity.type == 'electric-pole')) or entity.type == "pipe-to-ground" then
+		ghost = entity.surface.create_entity
+		{name="entity-ghost",	force=game.forces.Admins, inner_name="programmable-speaker", position=entity.position, direction = entity.direction}
+	else
+		ghost = entity.surface.create_entity
+		{name="entity-ghost",	force=game.forces.Admins, inner_name=entity.name, position=entity.position, direction = entity.direction}
+	end
+	ghost.last_user = game.players[event.player_index]
+end
+
+function modular_admin_ghosts_enable()
 	global.modular_admin_ghosts.enabled = true
 	modular_admin_add_submodule("modular_admin_ghosts")
 	for i, p in pairs(game.connected_players) do
 		modular_admin_ghosts_update_menu_button(p)
 	end
+	admin_create_force()
 end
 
-function modular_admin_ghosts_disable(mod)
+function modular_admin_ghosts_disable()
 	global.modular_admin_ghosts.enabled = false
+	modular_admin_remove_submodule("modular_admin_ghosts")
 	for i, p in pairs(game.connected_players) do
 		modular_admin_ghosts_update_menu_button(p)
 	end
-	modular_admin_remove_submodule("modular_admin_ghosts")
 end
 
 --
 --	EVENTS
 --
 
+Event.register(defines.events.on_preplayer_mined_item, modular_admin_ghosts_entity_mined)
+
 Event.register(-1, function(event)
 	if(global.modular_admin_ghosts.enabled) then
 		modular_admin_add_submodule("modular_admin_ghosts")
 	else
 		modular_admin_remove_submodule("modular_admin_ghosts")
+	end
+	admin_create_force()
+end)
+
+Event.register(defines.events.on_force_created, function(event)
+	if(event.force.name ~= "Admins") then
+		admin_create_force()
+		event.force.set_friendly(game.forces.Admins, true)
+		game.forces.Admins.set_cease_fire(event.force, true)
 	end
 end)
