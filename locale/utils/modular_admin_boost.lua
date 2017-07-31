84346c6ea72ab07b235f9be823e0106f2e74f1a2
Blackstone <IBlackI@users.noreply.github.com>
2017-07-31 22:52:27 +0200

    Started work on modular_admin_boost

--- locale/utils/modular_admin_boost.lua
+++ locale/utils/modular_admin_boost.lua
@@ -16,10 +16,13 @@
 
 global.modular_admin_boost = global.modular_admin_boost or {}
 global.modular_admin_boost.enabled = true
+global.modular_admin_boost.visable = global.modular_admin_boost.visable or {}
+global.modular_admin_boost.bonus_state = global.modular_admin_boost.bonus_state or {}
 
 --
 --	FUNCTIONS
 --
+
 function modular_admin_boost_enable(mod)
 	global.modular_admin_boost.enabled = true
 	modular_admin_add_submodule("modular_admin_boost")
@@ -36,9 +39,71 @@ function modular_admin_boost_disable(mod)
 	modular_admin_remove_submodule("modular_admin_boost")
 end
 
+
+function modular_admin_boost_update_menu_button(p)
+	if p.admin then
+		if global.modular_admin_boost.enabled then
+			if global.modular_admin_boost.visible[p.name] then
+				modular_admin_add_button(p.name, {name="modular_admin_boost_button", caption="Close character", order = 50, color = {r = 1, b = 0, g = 0}})
+			else
+				modular_admin_add_button(p.name, {name="modular_admin_boost_button", caption="Open character", order = 50, color = {r = 0, b = 0, g = 1}})
+			end
+		else 
+			modular_admin_remove_button(p.name, "modular_admin_boost_button")
+		end
+		modular_admin_boost_gui_changed(p)
+	end
+end
+
+function modular_admin_boost_gui_changed(p)
+	if p.admin then
+		bf = modular_admin_get_flow(p)
+		if global.modular_admin_boost.enabled then
+			if bf.modular_admin_boost_pane ~= nil then
+				mabp = bf.modular_admin_boost_pane
+				mabp.clear()
+			else
+				mabp = bf.add {type = "frame", name = "modular_admin_boost_pane", caption = "Specate Menu", direction = "vertical"}
+			end
+			mabp.style.visible = global.modular_admin_boost.visible[p.name]
+			srb = mabp.add {type = "button", name = "modular_admin_boost_return_button", caption = "Return"}
+			stb = mabp.add {type = "button", name = "modular_admin_boost_teleport_button", caption = "Teleport"}
+			srb.style.minimal_width = 150
+			stb.style.minimal_width = 150
+			if global.modular_admin_boost.follow_target[p.index] ~= nil then
+				srb.enabled = false
+				stb.enabled = false
+			else
+				srb.style.font_color = {r = 1, b = 0, g = 0}
+				stb.style.font_color = {r = 1, b = 0, g = 0}
+			end
+		else
+			if bf.modular_admin_boost_pane ~= nil then
+				bf.modular_admin_boost_pane.destroy()
+			end
+		end
+	end
+end
+
 --
 --	EVENTS
 --
+
+Event.register(defines.events.on_player_joined_game, function(event)
+	p = game.players[event.player_index]
+	if p.admin then
+		global.modular_admin_boost.bonus_state[p.name] = global.modular_admin_boost.bonus_state[p.name] or {}
+		if global.modular_admin_boost.enabled then
+			if global.modular_admin_boost.visable[p.name] then
+				modular_admin_add_button(p.name, {name="modular_admin_boost_button", caption="Close character", order = 50, color = {r = 1, b = 0, g = 0}})
+			else
+				modular_admin_add_button(p.name, {name="modular_admin_boost_button", caption="Open character", order = 50, color = {r = 0, b = 0, g = 1}})
+			end
+		else 
+			modular_admin_remove_button(p.name, "modular_admin_boost_button")
+		end
+	end
+end)
 
 Event.register(-1, function(event)
 	if(global.modular_admin_boost.enabled) then
@@ -47,3 +112,6 @@ Event.register(-1, function(event)
 		modular_admin_remove_submodule("modular_admin_boost")
 	end
 end)
+
+	
+--Event.register(defines.events.on_gui_click, modular_admin_boost_gui_clicked)
