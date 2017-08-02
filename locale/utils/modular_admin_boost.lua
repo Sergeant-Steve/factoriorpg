867cbaac171304069e6a397904b4d84129418902
Blackstone <IBlackI@users.noreply.github.com>
2017-08-02 16:45:01 +0200

    Made GUI modular_admin_boost

--- locale/utils/modular_admin_boost.lua
+++ locale/utils/modular_admin_boost.lua
@@ -16,8 +16,10 @@
 
 global.modular_admin_boost = global.modular_admin_boost or {}
 global.modular_admin_boost.enabled = true
-global.modular_admin_boost.visable = global.modular_admin_boost.visable or {}
+global.modular_admin_boost.visible = global.modular_admin_boost.visible or {}
 global.modular_admin_boost.bonus_state = global.modular_admin_boost.bonus_state or {}
+global.modular_admin_boost.active_color = {r = 1, b = 0, g = 1}
+global.modular_admin_boost.inactive_color = {r = 0, b = 0.5, g = 0}
 
 --
 --	FUNCTIONS
@@ -63,24 +65,75 @@ function modular_admin_boost_gui_changed(p)
 				mabp = bf.modular_admin_boost_pane
 				mabp.clear()
 			else
-				mabp = bf.add {type = "frame", name = "modular_admin_boost_pane", caption = "Specate Menu", direction = "vertical"}
+				mabp = bf.add {type = "frame", name = "modular_admin_boost_pane", caption = "Character Menu", direction = "vertical"}
 			end
 			mabp.style.visible = global.modular_admin_boost.visible[p.name]
-			srb = mabp.add {type = "button", name = "modular_admin_boost_return_button", caption = "Return"}
-			stb = mabp.add {type = "button", name = "modular_admin_boost_teleport_button", caption = "Teleport"}
-			srb.style.minimal_width = 150
-			stb.style.minimal_width = 150
-			if global.modular_admin_boost.follow_target[p.index] ~= nil then
-				srb.enabled = false
-				stb.enabled = false
+			bpb = mabp.add {type = "button", name = "modular_admin_boost_pickup_button", caption = "Pickup"}
+			bmb = mabp.add {type = "button", name = "modular_admin_boost_movement_button", caption = "Movement"}
+			bcb = mabp.add {type = "button", name = "modular_admin_boost_crafting_button", caption = "Crafting"}
+			brb = mabp.add {type = "button", name = "modular_admin_boost_reach_button", caption = "Reach"}
+			bib = mabp.add {type = "button", name = "modular_admin_boost_invincible_button", caption = "Invincible"}
+			bpb.style.minimal_width = 150
+			bmb.style.minimal_width = 150
+			bcb.style.minimal_width = 150
+			brb.style.minimal_width = 150
+			bib.style.minimal_width = 150
+			pbs = global.modular_admin_boost.bonus_state[p.name]
+			if pbs.pickup then
+				bpb.style.font_color = global.modular_admin_boost.active_color
 			else
-				srb.style.font_color = {r = 1, b = 0, g = 0}
-				stb.style.font_color = {r = 1, b = 0, g = 0}
+				bpb.style.font_color = global.modular_admin_boost.inactive_color 
+			end
+			if pbs.movement then
+				bmb.style.font_color = global.modular_admin_boost.active_color
+			else
+				bmb.style.font_color = global.modular_admin_boost.inactive_color 
+			end
+			if pbs.crafting then
+				bcb.style.font_color = global.modular_admin_boost.active_color
+			else
+				bcb.style.font_color = global.modular_admin_boost.inactive_color 
+			end
+			if pbs.reach then
+				brb.style.font_color = global.modular_admin_boost.active_color
+			else
+				brb.style.font_color = global.modular_admin_boost.inactive_color 
+			end
+			if pbs.invincible then
+				bib.style.font_color = global.modular_admin_boost.active_color
+			else
+				bib.style.font_color = global.modular_admin_boost.inactive_color 
 			end
 		else
 			if bf.modular_admin_boost_pane ~= nil then
 				bf.modular_admin_boost_pane.destroy()
 			end
+		end
+	end
+end
+
+function modular_admin_boost_gui_clicked(event)
+	if not (event and event.element and event.element.valid) then return end
+	local i = event.player_index
+	local p = game.players[i]
+	local e = event.element
+	if e ~= nil then
+		if e.name == "modular_admin_boost_button" then
+			global.modular_admin_boost.visible[p.name] = (not global.modular_admin_boost.visible[p.name])
+			modular_admin_boost_update_menu_button(p)
+		elseif e.parent.name == "modular_admin_boost_pane" then
+			if e.name == "modular_admin_boost_pickup_button" then
+				global.modular_admin_boost.bonus_state[p.name].pickup = not global.modular_admin_boost.bonus_state[p.name].pickup
+			elseif e.name == "modular_admin_boost_movement_button" then
+				global.modular_admin_boost.bonus_state[p.name].movement = not global.modular_admin_boost.bonus_state[p.name].movement
+			elseif e.name == "modular_admin_boost_crafting_button" then
+				global.modular_admin_boost.bonus_state[p.name].crafting = not global.modular_admin_boost.bonus_state[p.name].crafting
+			elseif e.name == "modular_admin_boost_reach_button" then
+				global.modular_admin_boost.bonus_state[p.name].reach = not global.modular_admin_boost.bonus_state[p.name].reach
+			elseif e.name == "modular_admin_boost_invincible_button" then
+				global.modular_admin_boost.bonus_state[p.name].invincible = not global.modular_admin_boost.bonus_state[p.name].invincible
+			end
+			modular_admin_boost_gui_changed(p)
 		end
 	end
 end
@@ -92,9 +145,9 @@ end
 Event.register(defines.events.on_player_joined_game, function(event)
 	p = game.players[event.player_index]
 	if p.admin then
-		global.modular_admin_boost.bonus_state[p.name] = global.modular_admin_boost.bonus_state[p.name] or {}
+		global.modular_admin_boost.bonus_state[p.name] = global.modular_admin_boost.bonus_state[p.name] or {pickup = false, movement = false, crafting = false, reach = false, invincible = false}
 		if global.modular_admin_boost.enabled then
-			if global.modular_admin_boost.visable[p.name] then
+			if global.modular_admin_boost.visible[p.name] then
 				modular_admin_add_button(p.name, {name="modular_admin_boost_button", caption="Close character", order = 50, color = {r = 1, b = 0, g = 0}})
 			else
 				modular_admin_add_button(p.name, {name="modular_admin_boost_button", caption="Open character", order = 50, color = {r = 0, b = 0, g = 1}})
@@ -113,5 +166,4 @@ Event.register(-1, function(event)
 	end
 end)
 
-	
---Event.register(defines.events.on_gui_click, modular_admin_boost_gui_clicked)
+Event.register(defines.events.on_gui_click, modular_admin_boost_gui_clicked)
