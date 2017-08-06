899d10c3ba43466d8a0b48a297f5783ae112c4fc
IBlackI <IBlackI@users.noreply.github.com>
2017-08-06 17:05:16 +0200

    On_tick! Not player_created

--- permissions.lua
+++ permissions.lua
@@ -36,5 +36,5 @@ function permissions_precheck(event)
 end
 
 Event.register(defines.events.on_player_created, permissions_precheck)
-Event.register(defines.events.on_player_created, permissions_upgrade)
+Event.register(defines.events.on_tick, permissions_upgrade)
 Event.register(-1, permissions_init)
