6e94f77f6303560e82d445c7e367443e2c471d2f
IBlackI <IBlackI@users.noreply.github.com>
2017-08-30 17:07:26 +0200

    Enabled correct modules

--- control.lua
+++ control.lua
@@ -1,22 +1,22 @@
 -- TOOLS: Recommend all be turned on
---require "locale/utils/event" --Yes this line is seriously commented out, and yes without this it won't even work. I guess you now HAVE TO change the settings to match what you need.
+require "locale/utils/event" --Yes this line is seriously commented out, and yes without this it won't even work. I guess you now HAVE TO change the settings to match what you need.
 require "mod-gui" --required for all other modules
 require "locale/utils/topgui" --utility module to be able to order the buttons in the top left
 require "locale/utils/char_mod"	--utility module to prevent multiple modules conflicting when modifying player bonus
-require "locale/utils/force_mod" --utility module to prevent multiple modules conflicting when modifying force bonus
+--CRASH TO DESKTOPrequire "locale/utils/force_mod" --utility module to prevent multiple modules conflicting when modifying force bonus
 --require "permissions" --Permission manager
 --require "trusted" --Module to add trusted players to a seperate permission group
 --require "locale/utils/patreon" --Module to give patreons spectate and a nice unique tag
 --require "locale/utils/admin"	--Admin module to give the admins spectate, commands and character modifications.
---require "announcements"	--Module to announce stuff ingame / give the players a welcome message
---require "tag" --Module to let players set a tag behind their names to improve teamwork
+require "announcements"	--Module to announce stuff ingame / give the players a welcome message
+require "tag" --Module to let players set a tag behind their names to improve teamwork
 --require "fmcd" --Module to consolidate saving data to an output file for the agent
 --require "stats" --Module to generate stats and print them to the filesystem
---require "popup" --Module to create and display an popup in the center of all players their screens.
+require "popup" --Module to create and display an popup in the center of all players their screens.
 --require "rules" --Module which displays a popup with the rules when a player joins, or presses the open rules button
 --require "rocket" --Module to stop people removing the rocket silo
---require "gravemarker" --Create a map tag on player death for easier corpse finding
---require "locale/utils/modular_admin" --New admin tools -untested
+require "gravemarker" --Create a map tag on player death for easier corpse finding
+require "locale/utils/modular_admin" --New admin tools -untested
 
 
 -- World Generators: Pick only ONE
