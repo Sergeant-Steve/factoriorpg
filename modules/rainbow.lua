67efe686f8ffceb666c256d146d607cd86db71d9
Brickcaster <dralon@yahoo.com>
2018-03-04 11:29:31 -0500

    Rainbow: Fixed offset
    Rainbow now has a greater offset so different players have different
    colors.

--- modules/rainbow.lua
+++ modules/rainbow.lua
@@ -7,7 +7,7 @@ global.rainbow = {}
 commands.add_command("rainbow", "Rainbow chat colors!", function()
     if game.player and game.player.admin then
         if not global.rainbow[game.player.name] then
-            global.rainbow[game.player.name] = 1800 * math.random() --This is an offset so everyone doesn't have the same color.
+            global.rainbow[game.player.name] = 1800000 * math.random() --This is an offset so everyone doesn't have the same color.
         else
             global.rainbow[game.player.name] = nil
             game.player.color = {}
@@ -21,12 +21,14 @@ function rainbow()
         if game.players[k] and game.players[k].connected then
             local player = game.players[k]
             local color = {
-                r = math.sin((game.tick + 133 + global.rainbow[game.player.name]) / 1700) * 127 + 127,
-                b = math.sin((game.tick + 266 + global.rainbow[game.player.name]) / 1800) * 127 + 127,
-                g = math.sin((game.tick + 500 + global.rainbow[game.player.name]) / 1900) * 127 + 127
+                r = math.sin((game.tick + global.rainbow[player.name]) / 1700) * 127 + 127,
+                b = math.sin((game.tick + 600 + global.rainbow[player.name]) / 1800) * 127 + 127,
+                g = math.sin((game.tick + 1200 + global.rainbow[player.name]) / 1900) * 127 + 127
             }
             player.color = color
-            color.alpha = 0.5
+            for k,v in pairs(color) do
+                color[k] = 127 + v/2
+            end
             player.chat_color = color
         end
     end
